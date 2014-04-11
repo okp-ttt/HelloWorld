0c9dd331f4a23b1105f5ae38b627e895ff4a5018
g12938oy <g12938oy@s30449.tsuda.ac.jp>
2014-04-11 11:03:32 +0900

    新規にHello作成しました

---
+++ src/hello/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author g12938oy
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
